7e73933befd448466fb85b5cc1ec6a4ee4d72435
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-05-13 23:17:51 +0600

    single data read

--- src/controller/product.ts
+++ src/controller/product.ts
@@ -1,23 +1,37 @@
 import type { IncomingMessage, ServerResponse, } from "http";
 import { readProduct } from "../servise/productServese";
+import type { IProduct } from "../types/product.types";
+
 
 export const product = (req: IncomingMessage, res: ServerResponse) => {
 
     const url = req.url;
     const method = req.method;
+    //product ==> /product/1 => []
 
+    const urlParts = url?.split("/")
+    // console.log(urlParts)
+    const id = urlParts && urlParts[1] === "product" ? Number(urlParts[2]) : null;
+    // console.log("this is a actual id :", id)
+
+
+
+
+    //Get all Product
     if (url === "/product" && method === "GET") {
-
-
-        // const product = [{
-        //     id: 1,
-        //     name: "musa -2",
-        // }];
 
         const product = readProduct();
 
         res.writeHead(200, { "content-type": "application/json" })
         res.end(JSON.stringify({ message: "product retrieved successful ", data: product }))
+
+    } else if (method === "GET" && id !== null) {
+        const products = readProduct();
+        const product = products.find((p: IProduct) => p.id === id)
+        res.writeHead(200, { "content-type": "application/json" })
+        res.end(JSON.stringify({ message: "product retrieved successful ", data: product }))
+
+
     }
 
 }
